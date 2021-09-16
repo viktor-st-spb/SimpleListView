e915fbc946e29b51170d04ed10608dddabdb1c87
viktor-st-spb <viktor.st.spb@gmail.com>
2021-09-16 08:15:16 +0300

    ViewBinding implemented

--- app/src/main/java/com/viktorstiblo/simplelistview/MainActivity.kt
+++ app/src/main/java/com/viktorstiblo/simplelistview/MainActivity.kt
@@ -2,18 +2,27 @@ package com.viktorstiblo.simplelistview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
 import android.widget.*
+import com.viktorstiblo.simplelistview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val myListView = findViewById<ListView>(R.id.lv_list_view)
-        val buttonNames = findViewById<Button>(R.id.btn_names)
-        val buttonFruits = findViewById<Button>(R.id.btn_fruits)
-        val buttonAnimals = findViewById<Button>(R.id.btn_animals)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+
+        setContentView(view)
+
+        //val myListView = findViewById<ListView>(R.id.lv_list_view)
+        //val buttonNames = findViewById<Button>(R.id.btn_names)
+        //val buttonFruits = findViewById<Button>(R.id.btn_fruits)
+        //val buttonAnimals = findViewById<Button>(R.id.btn_animals)
 
         /******** Created the list of Names *******/
         val nameList = listOf("Vick","Alex","Valery","Dmitriy","Vladimir","Vitaliy",
@@ -38,31 +47,31 @@ class MainActivity : AppCompatActivity() {
         fruitList.add("PineApple")
 
         /***** Button on click listener implemented ******/
-        buttonNames.setOnClickListener {
+        binding.btnNames.setOnClickListener {
             // Created an adapter
             val myNamesAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, nameList)
             // Assigned an adapter to list view
-            myListView.adapter = myNamesAdapter
+            binding.lvListView.adapter = myNamesAdapter
             // On item click listener with toast message
-            myListView.setOnItemClickListener { parent, view, position, id ->
+            binding.lvListView.setOnItemClickListener { parent, view, position, id ->
                 Toast.makeText(this, "Pressed Item: ${nameList[position]} ($position)",
                     Toast.LENGTH_SHORT).show()
             }
         }
 
-        buttonFruits.setOnClickListener {
+        binding.btnFruits.setOnClickListener {
             val myFruitsAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1,
                 fruitList)
 
-            myListView.adapter = myFruitsAdapter
+            binding.lvListView.adapter = myFruitsAdapter
 
-            myListView.setOnItemClickListener { parent, view, position, id ->
+            binding.lvListView.setOnItemClickListener { parent, view, position, id ->
                 Toast.makeText(this, "Pressed Item: ${fruitList[position]} ($position)",
                     Toast.LENGTH_SHORT).show()
             }
         }
 
-        buttonAnimals.setOnClickListener {
+        binding.btnAnimals.setOnClickListener {
 
             /****** Get animal list from res/values/animals.xml file ******/
             val animalList = resources.getStringArray(R.array.animals)
@@ -70,9 +79,9 @@ class MainActivity : AppCompatActivity() {
             val myAnimalAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1,
                 animalList)
 
-            myListView.adapter = myAnimalAdapter
+            binding.lvListView.adapter = myAnimalAdapter
 
-            myListView.setOnItemClickListener { parent, view, position, id ->
+            binding.lvListView.setOnItemClickListener { parent, view, position, id ->
                 Toast.makeText(this, "Pressed Item: ${animalList[position]} ($position)",
                     Toast.LENGTH_SHORT).show()
             }
